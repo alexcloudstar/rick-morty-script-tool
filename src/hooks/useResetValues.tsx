2d200fc57b6493fb0d2c337a3532e72a7c2ac16a
alexcloudstar <alexcloudstar@gmail.com>
2023-06-14 22:39:32 +0300

    fix runtime error

--- src/hooks/useResetValues.tsx
+++ src/hooks/useResetValues.tsx
@@ -1,18 +1,11 @@
 import { useInputStore, useSelectCharacter, useSelectLocation } from '../store';
 
 export const useResetValues = () => {
-  const [_location, setLocationValue] = useSelectLocation(state => [
-    state.value,
-    state.setValue,
-  ]);
-  const [_characters, setCharactersDropdownValue] = useSelectCharacter(
-    state => [_, state.setValue]
+  const setLocationValue = useSelectLocation(state => state.setValue);
+  const setCharactersDropdownValue = useSelectCharacter(
+    state => state.setValue
   );
-
-  const [_input, setInputValueStore] = useInputStore(state => [
-    state.value,
-    state.setValue,
-  ]);
+  const setInputValueStore = useInputStore(state => state.setValue);
 
   const resetValues = () => {
     setLocationValue('Please select location');
